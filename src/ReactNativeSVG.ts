2a0321c06a531ed2f50acaddaf7c813d7324b578
Tom Scallon <tscallon@fb.com>
2021-02-23 11:40:49 -0800

    Remove css.tsx, all it usages, and all its dependencies.

--- src/ReactNativeSVG.ts
+++ src/ReactNativeSVG.ts
@@ -24,13 +24,6 @@ import Mask from './elements/Mask';
 import Marker from './elements/Marker';
 import ForeignObject from './elements/ForeignObject';
 import { parse, SvgAst, SvgFromUri, SvgFromXml, SvgUri, SvgXml } from './xml';
-import {
-  SvgCss,
-  SvgCssUri,
-  SvgWithCss,
-  SvgWithCssUri,
-  inlineStyles,
-} from './css';
 import { LocalSvg, WithLocalSvg, loadLocalRawResource } from './LocalSvg';
 import {
   RNSVGCircle,
@@ -87,11 +80,6 @@ export {
   SvgFromXml,
   SvgUri,
   SvgXml,
-  SvgCss,
-  SvgCssUri,
-  SvgWithCss,
-  SvgWithCssUri,
-  inlineStyles,
   LocalSvg,
   WithLocalSvg,
   loadLocalRawResource,
